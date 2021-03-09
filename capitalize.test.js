41adbbe4242abfb05981c3bbe621b43349c5b170
Azryel <AzryelShroud@gmail.com>
2021-03-09 20:33:53 +0100

    write first test

---
+++ capitalize.test.js
@@ -0,0 +1,7 @@
+import { capitalize } from "./capitalize"
+
+test ("capitalize a string", () => {
+    expect (capitalize("capitalize")).toMatch("CAPITALIZE");
+    expect (capitalize("2")).toMatch("2");
+    expect (capitalize("number:1")).toMatch("NUMBER:1");
+} );
